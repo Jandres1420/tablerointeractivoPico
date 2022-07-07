4d3e3e93adfb6e6954cbaf278a974ae031e31248
juan pico <juan.pico-m@mail.escuelaing.edu.co>
2022-07-07 11:01:51 -0500

    Security login

--- src/main/java/co/edu/escuelaing/interactivebalckboardlife/entities/Ticket.java
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/entities/Ticket.java
@@ -1,5 +1,31 @@
 package co.edu.escuelaing.interactivebalckboardlife.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ticket {
-    
+    private static final Ticket instance = new Ticket();
+    private List<Integer> tickets;
+    public Ticket(){
+        tickets = new ArrayList<>();
+    }
+
+    public static Ticket getInstance() {
+        return instance;
+    }
+
+    public void checkTicket(){
+        
+    }
+
+    public List<Integer> getTickets() {
+        return this.tickets;
+    }
+
+    public void setTickets(List<Integer> tickets) {
+        this.tickets = tickets;
+    }
+
+
 }
+
